a13024079d8b19548e902d20ffe08857ff593103
Toni Guli <pllumiguli@icloud.com>
2021-08-22 11:51:46 +0200

    created alarm_clock::TimeZone, finished implementing a basic version of the parser, going to put it in lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,56 @@
+use alarm_clock::TimeZone;
 use getopt::Opt;
 
 fn main() {
-	rd();
+	parser();
 }
 
-fn rd() -> Result<(), Box<dyn std::error::Error>>{
-	let mut args: Vec<String> = std::env::args().collect();
+fn parser() -> Result<(), Box<dyn std::error::Error>> {
+	let args: Vec<String> = std::env::args().collect();
 	let mut opts = getopt::Parser::new(&args, "h:m:p:");
 
+	let mut hour_flag: Option<String> = None;
+	let mut minute_flag: Option<String> = None;
+	let mut is_pm: Option<TimeZone> = None;
+	
 	loop {
 		match opts.next().transpose()? {
 			None => break,
 			Some(opt) => {
 				match opt {
-					Opt('h', Some(hour)) => println!("h: {}", hour),
-					Opt('m', Some(minute)) => println!("m: {}", minute),
+					Opt('h', Some(hour)) => hour_flag = Some(hour),
+					Opt('m', Some(minute)) => minute_flag = Some(minute),
 					Opt('p', Some(pm)) => {
 						if &pm == "true" {
-							println!("p: true");
-						} else if &pm == "false" {
-							println!("p: false");
+							is_pm = Some(TimeZone::Pm);
 						} else {
-							println!("p: g");
+							is_pm = Some(TimeZone::Am);
 						}
 					},
-					Opt('p', None) => println!("p: None"),
 					_ => unreachable!()
 				}
 			}
 		}
 	}
+
+	let hour_flag: u8 = match hour_flag {
+		None => 0,
+		Some(h) => h.parse()?
+	};
+
+	let minute_flag: u8 = match minute_flag {
+		None => 0,
+		Some(m) => m.parse()?
+	};
+
+	let is_pm: TimeZone = match is_pm {
+		Some(TimeZone::Am) => TimeZone::Am,
+		_ => TimeZone::Pm
+	};
+
+	let config = (hour_flag, minute_flag, is_pm);
+
+	println!("{:?}", config);
 
 	Ok(())
 }
