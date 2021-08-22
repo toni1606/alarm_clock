fd7e1cd5db52d5c852c9766f2eb303aaa8bf1d86
Toni Guli <pllumiguli@icloud.com>
2021-08-22 11:29:30 +0200

    added basic working command-line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use getopt::Opt;
+
 fn main() {
-	println!("Hello, world!");
+	rd();
+}
+
+fn rd() -> Result<(), Box<dyn std::error::Error>>{
+	let mut args: Vec<String> = std::env::args().collect();
+	let mut opts = getopt::Parser::new(&args, "h:m:p:");
+
+	loop {
+		match opts.next().transpose()? {
+			None => break,
+			Some(opt) => {
+				match opt {
+					Opt('h', Some(hour)) => println!("h: {}", hour),
+					Opt('m', Some(minute)) => println!("m: {}", minute),
+					Opt('p', Some(pm)) => {
+						if &pm == "true" {
+							println!("p: true");
+						} else if &pm == "false" {
+							println!("p: false");
+						} else {
+							println!("p: g");
+						}
+					},
+					Opt('p', None) => println!("p: None"),
+					_ => unreachable!()
+				}
+			}
+		}
+	}
+
+	Ok(())
 }
